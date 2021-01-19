cbf491eecdfdea8f1963117c1656d56862fa165e
Keith McNulty <keith.mcnulty@gmail.com>
2021-01-19 22:47:26 +0000

    Add employee_performance

---
+++ R/data-employee_performance.R
@@ -0,0 +1,15 @@
+#' Employee performance data
+#'
+#' Fictional data on employee performance evaluation metrics for a group of salespeople.
+#'
+#' @format A dataframe with 366 rows and 5 variables:
+#' \describe{
+#' \item{sales}{The annual sales of the individual in millions of dollars}
+#' \item{new_customers}{The number of new customers acquired by the individual}
+#' \item{region}{The region the individuals works in - North, South, East or West}
+#' \item{gender}{The gender of the individual}
+#' \item{rating}{The performance rating of the individual - 1 = Low, 2 = Middle, 3 = High}
+#' }
+#' @examples
+#' employee_performance
+"employee_performance"
